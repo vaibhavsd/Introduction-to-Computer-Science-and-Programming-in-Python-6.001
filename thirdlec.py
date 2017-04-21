00d64229c7c18822137dca25828a981e47af82ba
vaibhavsd <vd99099@gmail.com>
2017-04-20 20:47:33 -0700

    lec 3

---
+++ thirdlec.py
@@ -0,0 +1,37 @@
+
+# Playing with the strings
+s= 'abcdef'
+
+print(s[-3])
+print(s[-1])
+print(s[0])
+
+# s[start:stop:step]
+print(s[1:6:1])
+print(s[::-1])
+
+#####################################################################
+for var in range(4):
+    print(1)
+
+for var in range(4,5):
+    print(2)
+
+#####################################################################
+s= "abcdefghiu"
+for n in range(len(s)):
+    if s[n]=="i" or s[n]=="u":
+        print("There is an i or u!")
+
+
+for n in s:
+    if n=='i' or n=='u':
+        print("There is an i or u!")
+#####################################################################
+
+# The Cheer Leader girls code
+
+#####################################################################
+
+# The number guessing code
+
