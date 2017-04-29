ca874f798562be408dbf742d72321359c686ccd6
Vaibhav Deshmukh (Student) <vdeshmuk@sundevils.asu.edu>
2017-04-28 17:34:07 -0700

    started assignment 5

---
+++ pset5/lec9.py
@@ -0,0 +1,11 @@
+# TO DO LIST
+
+# 1. She loves you dictionary algorithm
+#
+# 2. Bisection search without passing lists
+#
+# 3. Generate all subsets from a given list
+#
+# 4. Check if one list is the subset of the other
+#
+# 5. Create a list which is intersection of two lists
